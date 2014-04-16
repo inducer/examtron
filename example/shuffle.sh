ce6f35c87ee113e14e0f808f9d1ed76029721d12
Andreas Kloeckner <inform@tiker.net>
2014-04-15 19:28:46 -0500

    Minor shuffle fix

--- example/shuffle.sh
+++ example/shuffle.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 ../shuffle-exam --version=A --seed=17 sample-exam.tex > sample-a.tex
-../shuffle-exam --version=B --seed=17 sample-exam.tex > sample-b.tex
+../shuffle-exam --version=B --seed=19 sample-exam.tex > sample-b.tex
