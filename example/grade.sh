f4c3c6d8727fb6dd762026e5eef7870c9bcea512
Andreas Kloeckner <inform@tiker.net>
2014-04-16 16:55:43 -0500

    Add capability to read YAML responses

--- example/grade.sh
+++ example/grade.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-if test "$1" = ""; then
-  python3 ../grade-scantron scantron.dat --latex-key=out/answers-MY-A.dat --print-grades
-else
-  python3 ../grade-scantron scantron.dat --latex-key=out/answers-MY-A.dat "$@"
+CMD="$1"
+shift
+if test "$CMD" = ""; then
+  CMD="--print-grades"
 fi
+python3 ../grade-scantron \
+  scantron.dat \
+  response.yml \
+  --latex-key=out/answers-MY-A.dat \
+  --latex-key=out/answers-MY-B.dat \
+  "$CMD" "$@"
